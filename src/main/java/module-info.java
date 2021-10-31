6bfd62e1b48621fa0aa03f680a47a02ddbcff2f8
saxojeblin <dynamiicnr@gmail.com>
2021-10-31 13:32:39 -0400

    1.1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Nicholas Rubio
+ */
+
 module rubio.COP3330.assignment4.main {
     requires javafx.fxml;
     requires javafx.controls;
